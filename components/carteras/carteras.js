c83a9580decab048bff5a0eec2febc6fbd6fb7aa
TheChec <chechoosorio@hotmail.com>
2024-06-11 00:58:06 -0500

    fixcolor

--- components/carteras/carteras.js
+++ components/carteras/carteras.js
@@ -3,10 +3,11 @@ var isOpen = false;
 function navOpenAndClose(){
     let colors = document.getElementById('coloresCarteras')
     if(isOpen == false){
-        colors.style.height = 'fit-content'
+        let scrollHeight = colors.scrollHeight; // Obtenemos la altura completa del contenido
+        colors.style.height = scrollHeight + 'px';
         isOpen = true
     }else{
         colors.style.height = '0'
         isOpen = false
     }
-}
+} 

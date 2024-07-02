3ebdf2eed8d22d83d4e778be32ed16feeef638c2
hanannd1 <haanaan.2604@gmail.com>
2024-07-02 12:36:27 -0400

    solucionado

---
+++ components/forros-de-laptop/forros-de-laptop.js
@@ -0,0 +1,36 @@
+var isOpenNav = false
+function openAndCloseNav(){
+   let navSlide = document.getElementById("navSlide")
+    if (isOpenNav == false){
+        navSlide.style.display = "flex"
+        isOpenNav  = true
+    } else if(isOpenNav  == true){
+        navSlide.style.display = "none"
+        isOpenNav  = false
+    }
+}
+
+var isOpen = false;
+function navOpenAndClose(){
+    let colors = document.getElementById('coloresCarteras')
+    if(isOpen == false){
+        let scrollHeight = colors.scrollHeight; // Obtenemos la altura completa del contenido
+        colors.style.height = scrollHeight + 'px';
+        isOpen = true
+    }else{
+        colors.style.height = '0'
+        isOpen = false
+    }
+} 
+function appearBags(){
+    let carteras = document.getElementById("carteras")
+    let accesorios = document.getElementById("accesorios")
+    carteras.style.display = "grid" 
+    accesorios.style.display = "none"
+}
+function appearAccesories(){
+    let carteras = document.getElementById("carteras")
+    let accesorios = document.getElementById("accesorios")
+    carteras.style.display = "none"
+    accesorios.style.display = "grid"
+}

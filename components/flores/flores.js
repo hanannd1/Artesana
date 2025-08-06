38b965674e49b88f90696bbaa14f9edd518e92f0
Hanan.nd1 <haanaan.2604@gmail.com>
2025-08-06 02:52:47 -0400

    json

--- components/flores/flores.js
+++ components/flores/flores.js
@@ -26,22 +26,6 @@ function openAndCloseNav() {
     }
 }
 
-function apearAndDesapper(showid, hideid1, hideid2, hideid3){
-    let show = document.getElementById(`${showid}`);
-    let hide1 = document.getElementById(`${hideid1}`);
-    let hide2 = document.getElementById(`${hideid2}`);
-    let hide3 = document.getElementById(`${hideid3}`);
-
-    animateContainer(showid)
-
-    show.style.display = "grid";
-
-    hide1.style.display = "none";
-    hide2.style.display = "none";
-    hide3.style.display = "none"
-}
-
-
 function animateContainer(containerId) {
     const container = document.getElementById(containerId);
     if (!container) return;
@@ -61,5 +45,79 @@ sr.reveal(".cuerpoVista",{
     duration: 2000, 
     origin: "left",
     distance: "150px",
-    reset: true
+    reset: false
 })
+function apearAndDesapper(...categorias) {
+    const [mostrarId, ...ocultarIds] = categorias;
+
+    const mostrar = document.getElementById(mostrarId);
+    if (mostrar) {
+        animateContainer(mostrarId);
+        mostrar.style.display = "grid";
+    }
+
+    ocultarIds.forEach(id => {
+        const ocultar = document.getElementById(id);
+        if (ocultar) {
+            ocultar.style.display = "none";
+        }
+    });
+}
+
+fetch('./../../productos/flores.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al cargar el archivo JSON');
+        }
+        return response.json();
+    })
+    .then(productos => {
+        const categorias = []
+        const contenedorCategorias = document.getElementById("botones");
+        productos.forEach(producto => {
+
+            if (!categorias.includes(producto.categoria)) {
+                categorias.push(producto.categoria)
+            }
+
+            const contenedor = document.getElementById(producto.categoria);
+
+
+            const tarjeta = document.createElement("div");
+            tarjeta.classList.add("tarjeta");
+            const descripcionHTML = producto.descripcion
+                ? `<h2>${producto.descripcion}</h2>`
+                : '';
+
+            tarjeta.innerHTML = `
+        <img src="${producto.img}" alt="${producto.titulo}">
+        <h3>${producto.titulo}</h3>
+        ${descripcionHTML}
+        <h3>${producto.precio}</h3>
+      `;
+
+            contenedor.appendChild(tarjeta);
+        });
+
+        categorias.forEach((cat, index) => {
+            const contenedor = document.getElementById(cat);
+            contenedor.style.display = index === 0 ? "grid" : "none"; 
+        });
+
+
+        categorias.forEach((categoriaSeleccionada) => {
+            const otrasCategorias = categorias.filter(c => c !== categoriaSeleccionada);
+            const argumentos = [categoriaSeleccionada, ...otrasCategorias]
+                .map(cat => `'${cat}'`)
+                .join(", ");
+
+            const boton = document.createElement("button");
+            boton.textContent = categoriaSeleccionada.charAt(0).toUpperCase() + categoriaSeleccionada.slice(1);
+            boton.setAttribute("onclick", `apearAndDesapper(${argumentos})`);
+            contenedorCategorias.appendChild(boton);
+        });
+
+    })
+    .catch(error => {
+        console.error('Hubo un problema con la carga del JSON:', error);
+    });
--- components/llaveros/llaveros.js
+++ components/llaveros/llaveros.js
@@ -3,37 +3,28 @@ var isOpenNav = false
 function openAndCloseNav() {
     let navSlide = document.getElementById("navSlide");
     if (!isOpenNav) {
-        navSlide.classList.add("open"); 
+        navSlide.classList.add("open");
         isOpenNav = true;
     } else {
-        navSlide.classList.remove("open"); 
+        navSlide.classList.remove("open");
         isOpenNav = false;
     }
 }
 
 var isOpen = false;
-function navOpenAndClose(){
+function navOpenAndClose() {
     let colors = document.getElementById('coloresCarteras')
-    if(isOpen == false){
+    if (isOpen == false) {
         let scrollHeight = colors.scrollHeight; // Obtenemos la altura completa del contenido
         colors.style.height = scrollHeight + 'px';
         isOpen = true
-    }else{
+    } else {
         colors.style.height = '0'
         isOpen = false
     }
-} 
-function apearAndDesapper(showid, hideid1, hideid2){
-    let show = document.getElementById(`${showid}`);
-    let hide1 = document.getElementById(`${hideid1}`);
-    let hide2 = document.getElementById(`${hideid2}`);
-
-    animateContainer(showid)
-    show.style.display = "grid";
-    hide1.style.display = "none";
-    hide2.style.display = "none";
-    
 }
+
+
 
 function animateContainer(containerId) {
     const container = document.getElementById(containerId);
@@ -46,14 +37,90 @@ function animateContainer(containerId) {
     setTimeout(() => {
         container.style.opacity = 1;
         container.style.transform = "translateX(0)";
-    }, 50); 
+    }, 50);
 }
 
 
 
-sr.reveal(".cuerpoVista",{
-    duration: 2000, 
+sr.reveal(".cuerpoVista", {
+    duration: 2000,
     origin: "left",
     distance: "150px",
-    reset: true
+    reset: false
 })
+
+function apearAndDesapper(...categorias) {
+    const [mostrarId, ...ocultarIds] = categorias;
+
+    const mostrar = document.getElementById(mostrarId);
+    if (mostrar) {
+        animateContainer(mostrarId);
+        mostrar.style.display = "grid";
+    }
+
+    ocultarIds.forEach(id => {
+        const ocultar = document.getElementById(id);
+        if (ocultar) {
+            ocultar.style.display = "none";
+        }
+    });
+}
+
+fetch('./../../productos/llaveros.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al cargar el archivo JSON');
+        }
+        return response.json();
+    })
+    .then(productos => {
+        const categorias = []
+        const contenedorCategorias = document.getElementById("botones");
+        productos.forEach(producto => {
+
+            if (!categorias.includes(producto.categoria)) {
+                categorias.push(producto.categoria)
+            }
+
+            const contenedor = document.getElementById(producto.categoria);
+
+
+            const tarjeta = document.createElement("div");
+            tarjeta.classList.add("tarjeta");
+            const descripcionHTML = producto.descripcion
+                ? `<h2>${producto.descripcion}</h2>`
+                : '';
+
+            tarjeta.innerHTML = `
+        <img src="${producto.img}" alt="${producto.titulo}">
+        <h3>${producto.titulo}</h3>
+        ${descripcionHTML}
+        <h3>${producto.precio}</h3>
+      `;
+
+            contenedor.appendChild(tarjeta);
+        });
+
+        categorias.forEach((cat, index) => {
+            const contenedor = document.getElementById(cat);
+            contenedor.style.display = index === 0 ? "grid" : "none"; 
+        });
+
+
+        categorias.forEach((categoriaSeleccionada) => {
+            const otrasCategorias = categorias.filter(c => c !== categoriaSeleccionada);
+            const argumentos = [categoriaSeleccionada, ...otrasCategorias]
+                .map(cat => `'${cat}'`)
+                .join(", ");
+
+            const boton = document.createElement("button");
+            boton.textContent = categoriaSeleccionada.charAt(0).toUpperCase() + categoriaSeleccionada.slice(1);
+            boton.setAttribute("onclick", `apearAndDesapper(${argumentos})`);
+            contenedorCategorias.appendChild(boton);
+        });
+
+    })
+    .catch(error => {
+        console.error('Hubo un problema con la carga del JSON:', error);
+    });
+
